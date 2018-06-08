45c48370f14b83a52262660bfbbb804995c09577
sokhasen <sensokha.master@gmail.com>
2018-06-08 11:35:04 +0700

    user management module

--- app.js
+++ app.js
@@ -5,21 +5,28 @@ var mongoose = require("mongoose");
 var DEFAULT_PORT = process.env.PORT || 2018;
 var STAGE = "/api/v1/";
 
-const mongoLocal = "mongodb://sokha:76b16afb8783fecd803b737acaaed18d41b9aa86c6900eb6a0ac852be132805a@ds213229.mlab.com:13229/mock-server-api"; //
-// const mongoLocal = 'mongodb://localhost/api_management';
+// const mongoLocal = "mongodb://sokha:76b16afb8783fecd803b737acaaed18d41b9aa86c6900eb6a0ac852be132805a@ds213229.mlab.com:13229/mock-server-api"; //
+const mongoLocal = 'mongodb://localhost/api_management';
 mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || mongoLocal);
 var db = mongoose.connection;
 
 var user = require("./routes/user");
-
 var workspace = require("./routes/workspace");
 var resource = require("./routes/resource");
 var method = require("./routes/method");
+
+const registerRouters =  [user, workspace, resource, method];
 
 var middleware = require("./middlewares/middleware");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // app.use(middleware.checkSubDomainName);
 /*
@@ -33,7 +40,7 @@ app.use((req, res, next) => {
 });
 */
 
-app.use(STAGE, [user, workspace, resource, method]);
+app.use(STAGE, middleware.guardAuth,  registerRouters);
 
 
 // catch 404 and forward to error handler
--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const User = require('../models/user');
 
 module.exports.checkSubDomainName =  function ( req, res, next) {
@@ -20,6 +21,40 @@ module.exports.checkSubDomainName =  function ( req, res, next) {
                 });
             }
         });
+    }
+};
+
+module.exports.guardAuth  =  function (req, res, next) {
+    // skip guard authorization
+    if (req.path === "/users/login" || req.path === "/users/register") {
+        next();
+    }
+    else {
+        const token = req.body.token || req.query.token || req.headers['x-access-token'];
+        // decode token
+        if (token) {
+
+            // verifies secret and checks exp
+            jwt.verify(token, 'secret', function(err, decoded) {
+                if (err) {
+                    return res.json({ success: false, message: 'Failed to authenticate token.' });
+                } else {
+                    // if everything is good, save to request for use in other routes
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+
+        } else {
+
+            // if there is no token
+            // return an error
+            return res.status(403).send({
+                success: false,
+                message: 'No token provided.'
+            });
+
+        }
     }
 
 };
